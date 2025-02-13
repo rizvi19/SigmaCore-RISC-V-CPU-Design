d3220634bce1ae5d0416a14b962a1894098e932f
rizvi19 <shahriar10rizvi@gmail.com>
2025-02-13 18:03:17 +0600

    Add SRAM module and implement register file with read/write operations

--- RTL/Memory/register_file.sv
+++ RTL/Memory/register_file.sv
@@ -7,21 +7,44 @@
 
 `timescale 1ns/1ps
 module regfile(
-    // Clock and Reset
+    // Clock and Reset Ports
     input logic clk,
     input logic reset,
 
-    // Read
+    // Read Ports
     input logic [4:0] read1_addr,
     input logic [4:0] read2_addr,
     output logic [31:0] read1_data,
     output logic [31:0] read2_data,
 
-    // Write
+    // Write Ports
     input logic [4:0] write_addr,
     input logic [31:0] write_data,
     input logic write_enable,
 );
 
 
-logic 
+// Packed Array declaration for 32bit x 32 registers
+logic [31:0] registers [31:0]; 
+
+// Write Operation
+always @(posedge clk) begin
+    if(reset == 1'b0) begin 
+        for(int i=0; i<32; i++) begin
+            registers[i] <= 32'b0;
+        end
+    end
+    // x0 register is Zero Register and only can be 0, so it can't be written with another value
+    else if(write_enable == 1'b1 && write_addr != 5'b0) begin
+        registers[write_addr] <= write_data;
+    end
+end
+
+// Read Operation
+always_comb begin
+    read1_data = registers[read1_addr];
+    read2_data = registers[read2_addr];
+end
+
+endmodule
+
