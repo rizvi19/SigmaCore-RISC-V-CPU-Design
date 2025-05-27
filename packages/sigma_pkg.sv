564188ffe5b6c5f3db35139660ef21159dd14672
rizvi19 <shahriar10rizvi@gmail.com>
2025-05-28 01:08:57 +0600

    Packages modified

--- packages/sigma_pkg.sv
+++ packages/sigma_pkg.sv
@@ -2,23 +2,13 @@
 
 package sigma_pkg; 
 
-    // Define for ALU Operations
-    typedef enum logic [3:0] {
-        ALU_ADD  = 4'b0000,
-        ALU_SUB  = 4'b0001,
-        ALU_AND  = 4'b0010,
-        ALU_OR   = 4'b0011,
-        ALU_XOR  = 4'b0100,
-        ALU_SLL  = 4'b0101,
-        ALU_SRL  = 4'b0110,
-        ALU_SRA  = 4'b0111, 
-        ALU_SLT  = 4'b1000,
-        ALU_SLTU = 4'b1001
-    } alu_op_e;
-
-    // Define types for Sign Extender
-    typedef enum logic [1:0] {
-        TYPE_I = 2'b00
-    } ins_type;
+  parameter logic [3:0] ALU_ADD = 4'b0000;
+  parameter logic [3:0] ALU_SUB = 4'b0001;
+  parameter logic [3:0] ALU_AND = 4'b0010;
+  parameter logic [3:0] ALU_OR  = 4'b0011;
+  parameter logic [3:0] ALU_XOR = 4'b0100;
+  parameter logic [3:0] ALU_SLL = 4'b0101; 
+  parameter logic [3:0] ALU_SRL = 4'b0110; 
+  parameter logic [3:0] ALU_SRA = 4'b0111; 
 
 endpackage
